24bb3d13373f8d380bfcfd3286428d8f49874640
Ture <josetureganobarbero@gmail.com>
2019-04-11 18:19:01 +0200

    Primer commit

---
+++ src/test/java/es/panel/curso/java/selenium/AppTest.java
@@ -0,0 +1,19 @@
+package es.panel.curso.java.selenium;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+
+
+@Epic("Alumno Tests Epic")
+@Feature("Super Features")
+class AppTest {
+
+	@Test
+	void testConstructorAlumno() {
+		assertEquals(false, false);
+	}
+}
