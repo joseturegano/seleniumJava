8e095733ec5a4403046e5ddfc387ad381b339143
Ture <josetureganobarbero@gmail.com>
2019-04-11 20:37:58 +0200

    MI primera prueba.

--- src/test/java/es/panel/curso/java/selenium/AppTest.java
+++ src/test/java/es/panel/curso/java/selenium/AppTest.java
@@ -3,6 +3,10 @@ package es.panel.curso.java.selenium;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
@@ -14,6 +18,22 @@ class AppTest {
 
 	@Test
 	void testConstructorAlumno() {
+		//System.setProperty("webdriver.chrome.driver", "D:\\ENVIRONMENT\\selenium\\chromedriver\\chromedriver.exe");
+		// Clave Valor
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com/xhtml");
+		WebElement home = driver.findElement(By.name("q"));
+		home.sendKeys("Curso Java");
+		home.submit();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.quit();
 		assertEquals(false, false);
 	}
 }
