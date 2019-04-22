a4d1c62b168e4e97dd1d00916d048385798278b9
Ture <josetureganobarbero@gmail.com>
2019-04-22 19:29:19 +0200

    Ejecucion de pruebas en paralelo.

--- src/test/java/es/panel/curso/java/selenium/AppTest.java
+++ src/test/java/es/panel/curso/java/selenium/AppTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,6 +14,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 
+@Execution(ExecutionMode.CONCURRENT)
 
 @Epic("Alumno Tests Epic")
 @Feature("Super Features")
